1e7139f1be48f6aefffeb5e3357495d101c25738
sravanikancharla455 <65275412+sravanikancharla455@users.noreply.github.com>
2020-05-18 18:32:17 +0530

    Add files via upload

---
+++ ondemandcarwash/src/main/java/com/project/ondemandcarwash/OndemandcarwashApplication.java
@@ -0,0 +1,13 @@
+package com.project.ondemandcarwash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OndemandcarwashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OndemandcarwashApplication.class, args);
+	}
+
+}
